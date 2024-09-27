25ff70f61318c6f9f1650c58d930f7608b332635
M-12-10 <buttwahaj110@gmail.com>
2024-09-28 04:42:58 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Image data and captions
 const images = [
-  { src: "image1.jpg", caption: "1. Person 1 - Wahaj loves you!" },
-  { src: "image2.jpg", caption: "2. Person 2 - Trusted and loved!" },
-  { src: "image3.jpg", caption: "3. Person 3 - You're awesome!" },
-  { src: "image4.jpg", caption: "4. Person 4 - A special one!" },
-  { src: "image5.jpg", caption: "5. Person 5 - Forever in trust!" },
-  { src: "image6.jpg", caption: "6. Person 6 - You're in Wahaj's heart!" },
-  { src: "image7.jpg", caption: "7. Person 7 - Loved deeply!" },
-  { src: "image8.jpg", caption: "8. Person 8 - Wahaj appreciates you!" },
-  { src: "image9.jpg", caption: "9. Person 9 - You're a rockstar!" },
-  { src: "image10.jpg", caption: "10. Person 10 - Always trusted!" }
+  { src: "image1.jpg", caption: "10. Janaab e Aala" },
+  { src: "image2.jpg", caption: "9. Azeeza" },
+  { src: "image3.jpg", caption: "8. Cutu" },
+  { src: "image4.jpg", caption: "7. Pyaru" },
+  { src: "image5.jpg", caption: "6. Fvrt Chngri" },
+  { src: "image6.jpg", caption: "5. Rapunzel" },
+  { src: "image7.jpg", caption: "4. Love of my Life" },
+  { src: "image8.jpg", caption: "3. Future Wife" },
+  { src: "image9.jpg", caption: "2. Meri Jaan" },
+  { src: "image10.jpg", caption: "1. Maryam Rajput" }
 ];
 
 let currentIndex = 0;
